07d9dfe58f0309f2202ad676270e7b6ec7ce8995
SakthiBharathiPCMR <sakthibharathipcmr@gmail.com>
2024-11-11 19:45:45 +0530

    2

---
+++ Assets/Scripts/EarthScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EarthScript : MonoBehaviour
+{
+
+    private const float rotateDegree = -360f;
+
+    public float rotateTime = 5f;
+    public float scaleTime = 2f;
+    public float scaleAmount = 0.8f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.DORotate(Vector3.forward * rotateDegree, rotateTime, RotateMode.FastBeyond360)
+        .SetLoops(-1, LoopType.Restart)
+        .SetEase(Ease.Linear);
+
+        transform.DOScale((Vector2.one * scaleAmount), scaleTime)
+        .SetLoops(-1, LoopType.Yoyo)
+        .SetEase(Ease.Linear);
+
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/FollowEarth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowEarth : MonoBehaviour
+{
+    private int currentHealth;
+
+    public int health = 3;
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        damegeHealth();
+        if(currentHealth<=0)
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+
+    private void damegeHealth()
+    {
+        currentHealth--;
+    }
+
+
+}
